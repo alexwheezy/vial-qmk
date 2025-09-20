c192dae79959e04138cdae85a310905c8aa96c75
Alexander Alekhin <sphrand@gmail.com>
2025-09-20 16:46:18 +0300

    update the keymap config

--- keyboards/ergohaven/imperial44/keymaps/gallium/keymap.c
+++ keyboards/ergohaven/imperial44/keymaps/gallium/keymap.c
@@ -3,6 +3,14 @@
 
 void lang_toggle(void);
 
+enum user_codes {
+    // Macros
+    RBT_DWM = SAFE_RANGE,
+    RBT,
+    DEL_WRD,
+    SLANG,
+};
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     case RBT:
@@ -47,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [_LOWER] = LAYOUT(
          KC_GRV,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                      KC_Y,    KC_U,            KC_I,          KC_O,         KC_P,          KC_LBRC,
          _______, LGUI_T(KC_A), LALT_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G,                      KC_H,    RCTL_T(KC_J),    RSFT_T(KC_K),  RALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
-         KC_NO,   KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,                      KC_N,    KC_M,            KC_COMM,       KC_DOT,       KC_RBRC,       _______,
+         LG_WORD, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,                      KC_N,    KC_M,            KC_COMM,       KC_DOT,       KC_RBRC,       _______,
                                                       _______, _______, _______, KC_NO,    KC_NO, _______, _______, _______
         ),
 
